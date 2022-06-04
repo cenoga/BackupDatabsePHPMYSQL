2b644add22cf265b490b7ac91f5fd1b76243902a
Christian Noga <36398022+cenoga@users.noreply.github.com>
2022-06-04 22:45:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Backup Database in PHP</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+  <div class="row">
+    <div class="col-md-8 offset-md-2">
+      <div class="card">
+      <div class="card-header bg">
+        <h1>Backup Database in PHP</h1>
+      </div>
+          <div class="card-body">
+         <a href="backup.php" class="btn btn-success">Backup Database</a>
+          </div>
+      </div>
+    </div>
+  </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
